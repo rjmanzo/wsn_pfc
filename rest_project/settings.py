bdb4616c066a7c1f783456b743c9570bedf17953
Renato Manzo <manzorenato@gmail.com>
2017-03-06 00:51:22 -0300

    modificando settings

--- rest_project/settings.py
+++ rest_project/settings.py
@@ -106,14 +106,27 @@ USE_TZ = False
 
 #esto esta en la documentacion de django. Como seleccionar y apuntar a los directoreos estaticos
 #BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-MEDIA_ROOT = 'media'
-STATIC_ROOT = 'static'
-MEDIA_URL = '/media'
-STATIC_URL = '/static'
 
 STATICFILES_DIR = (
     os.path.join(BASE_DIR,'static')
 )
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 #Cors settings
 CORS_ORIGIN_ALLOW_ALL = True
