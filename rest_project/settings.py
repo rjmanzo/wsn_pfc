98583686e58ee0b5cd83b77cfd1cae52f837093d
Renato Manzo <manzorenato@gmail.com>
2017-03-09 17:54:47 -0300

    si

--- rest_project/settings.py
+++ rest_project/settings.py
@@ -116,19 +116,20 @@ USE_TZ = False
 
 #esto esta en la documentacion de django. Como seleccionar y apuntar a los directoreos estaticos
 #PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+#BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 #STATIC_URL = 'staticfiles/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 #STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static')
+    os.path.join(BASE_DIR,'wsn','static'),
+    #'wsn/static/',
 ]
 
 # Simplified static file serving.
